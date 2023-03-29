46dda1a03f88de36c1cc953770f406868d0fc9e8
sangjuneeeee <wns1169@gmail.com>
2023-03-30 01:53:35 +0900

    js

---
+++ practice for project2-1/javascript/jsexample.js
@@ -0,0 +1,9 @@
+let a = 5;//let은 변수, (var 도 있지만 이건 옛날 거. 쓰지말고 걍 let 쓰면 됨)
+const b = 2;//const는 상수
+
+const c = null;
+let somthing;
+
+alert("박상준 그는 천잰가? \n(맞다면 확인)");
+console.log(a + b + "a");//콘솔창에 출력
+console.log(null, somthing);
