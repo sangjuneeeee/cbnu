b7f3eac3987d524f3d4e712e56f4bb5a26da115c
sangjuneeeee <wns1169@gmail.com>
2023-04-03 15:39:47 +0900

    js

--- practice for project2-1/javascript/jslogin.js
+++ practice for project2-1/javascript/jslogin.js
@@ -3,5 +3,6 @@
 // const loginInput = loginForm.querySelector("input");
 // const loginButton = loginForm.querySelector("button");
 
-const loginInput = document.querySelector("#login-form input");//1번 코드를 이 처럼 나타낼 수 있음
-const loginButton = document.querySelector("#login-form button");
+// const loginInput = document.querySelector("#login-form input");//1번 코드를 이 처럼 나타낼 수 있음
+// const loginButton = document.querySelector("#login-form button");
+//~~~{}등등//안쓰는 이유 : js로도 가능하지만 별로 안이쁨, 비효율 > html을 최대한 활용 후 이용
