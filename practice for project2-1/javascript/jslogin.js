101c74006983f6f51664770976a10a22e5d22a1f
sangjuneeeee <wns1169@gmail.com>
2023-04-03 14:31:14 +0900

    js

---
+++ practice for project2-1/javascript/jslogin.js
@@ -0,0 +1,7 @@
+//1.
+// const loginForm = document.getElementById("login-form");
+// const loginInput = loginForm.querySelector("input");
+// const loginButton = loginForm.querySelector("button");
+
+const loginInput = document.querySelector("#login-form input");//1번 코드를 이 처럼 나타낼 수 있음
+const loginButton = document.querySelector("#login-form button");
