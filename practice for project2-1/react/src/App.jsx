7cfffb1725c91079ce3496763d5e63f5f3dccd3a
sangjuneeeee <wns1169@gmail.com>
2023-04-11 22:39:09 +0900

    react

--- practice for project2-1/react/src/App.jsx
+++ practice for project2-1/react/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          sangjun iq {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
